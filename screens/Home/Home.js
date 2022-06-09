0b2a9aaefaf334328952e38cd02ede58f127e2bb
CharaD7 <jijakahn6@gmail.com>
2022-06-09 07:21:45 +0000

    Completed Search TextInput

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	View,
 	Text,
@@ -10,15 +10,76 @@ import {
 import { FONTS, SIZES, COLORS, dummyData, icons } from "../../constants";
 
 const Home = () => {
+
+	const [selectedCategoryId, setSelectedCategoryId] = useState(1);
+	const [selectedMenuType, setSelectedMenuType] = useState(1);
+	const [menuList, setMenuList] = useState([]);
+
+	function renderSearch() {
+		return (
+			<View
+				style={{
+					flexDirection : 'row',
+					height : 40,
+					alignItems : 'center',
+					marginHorizontal : SIZES.padding,
+					marginVertical : SIZES.base,
+					paddingHorizontal : SIZES.radius,
+					borderRadius : SIZES.radius,
+					backgroundColor : COLORS.lightGray2
+				}}
+			>
+			
+			{/* Icon */}
+			<Image 
+				source={icons.search}
+				style={{
+					height : 20,
+					width : 20,
+					tintColor : COLORS.black
+				}}
+			/>
+			
+			{/* Text Input */}
+			<TextInput 
+				style={{
+					flex : 1,
+					marginLeft : SIZES.radius,
+					marginVertical : -5,
+					...FONTS.body3
+				}}
+				placeholder="search food..."
+			/>
+			
+			{/* Filter */}
+			<TouchableOpacity
+				onPress={() => {}}
+			>
+				<Image 
+					source={icons.filter}
+					style={{
+						height : 20,
+						width : 20,
+						tintColor : COLORS.black
+					}}
+				/>
+			</TouchableOpacity>
+			
+			</View>
+		)
+	}
+
 	return (
 		<View
 			style={{
 				flex: 1,
-				alignItems: 'center',
-				justifyContent: 'center'
 			}}
 		>
-			<Text>Home</Text>
+			{/* Search */}
+			{renderSearch()}
+
+
+			{/* List */}
 		</View>
 	)
 }
